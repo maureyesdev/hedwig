815b4b06c2ab1fcc21a66867ca84dceb3697ca4e
Mauricio Reyes <maureyes.dev@gmail.com>
2025-01-06 09:55:41 -0600

    feat: add a request factory in order to handle curl and http syntax

--- lua/hedwig/init.lua
+++ lua/hedwig/init.lua
@@ -11,6 +11,38 @@ local function trim_string(s)
   return s:match("^%s*(.-)%s*$")
 end
 
+-- Request factory to handle curl request and HTTP syntax request
+local function request_factory(lines)
+  -- TODO: add validation if curl and if not then assume it's HTTP syntax
+  return {
+    execute = function()
+      -- Combine lines into a single command
+      local request = table.concat(lines, "\n")
+
+      -- Handle line continuations (`\`) and trim excess whitespace
+      request = request:gsub("\\%s*\n", " "):gsub("\n", " ")
+      request = trim_string(request)
+
+      -- Add the '-s' flag if not present
+      if not request:find("%-s") then
+        request = request .. " -s"
+      end
+
+      -- Execute the curl command
+      local output = vim.fn.systemlist(request)
+      return output
+    end,
+  }
+end
+
+-- Open a new vertical split and display the output
+local function display_output(output)
+  vim.api.nvim_command("vsplit")
+  local bfrnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(0, bfrnr)
+  vim.api.nvim_buf_set_lines(bfrnr, 0, -1, false, output)
+end
+
 function M.setup()
   -- Support for .http and .rest files
   vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
@@ -25,26 +57,11 @@ function M.run()
   -- Read the current buffer
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-  -- Combine lines into a single command
-  local request = table.concat(lines, "\n")
+  local request = request_factory(lines)
+  local output = request.execute()
 
-  -- Handle line continuations (`\`) and trim excess whitespace
-  request = request:gsub("\\%s*\n", " "):gsub("\n", " ")
-  request = trim_string(request)
-
-  -- Add the '-s' flag if not present
-  if not request:find("%-s") then
-    request = request .. " -s"
-  end
-
-  -- Execute the curl command
-  local output = vim.fn.systemlist(request)
-
-  -- Open a new vertical split and display the output
-  vim.api.nvim_command("vsplit")
-  local bfrnr = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(0, bfrnr)
-  vim.api.nvim_buf_set_lines(bfrnr, 0, -1, false, output)
+  -- TODO: Probably need to handle more ways to display the output
+  display_output(output)
 end
 
 return M
